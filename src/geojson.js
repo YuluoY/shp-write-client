b8c3f5917e8eed0d40d5d3611d111b0b76678fd2
Yuluo <568055454@qq.com>
2024-03-21 12:33:22 +0800

    init

---
+++ src/geojson.js
@@ -0,0 +1,37 @@
+module.exports.point = justType('Point', 'POINT');
+module.exports.line = justType('LineString', 'POLYLINE');
+module.exports.polygon = justType('Polygon', 'POLYGON');
+
+function justType(type, TYPE) {
+    return function(gj) {
+        var oftype = gj.features.filter(isType(type));
+        var geometries;
+
+        if (TYPE === 'POLYGON') {
+          geometries = [oftype.map(justCoords)];
+        } else if (TYPE === 'POLYLINE') {
+          geometries = oftype.map(function(t) {
+            return [justCoords(t)];
+          });
+        } else {
+          geometries = oftype.map(justCoords);
+        }
+        return {
+            geometries: geometries,
+            properties: oftype.map(justProps),
+            type: TYPE
+        };
+    };
+}
+
+function justCoords(t) {
+    return t.geometry.coordinates;
+}
+
+function justProps(t) {
+    return t.properties;
+}
+
+function isType(t) {
+    return function(f) { return f.geometry.type.replace('Multi', '') === t; };
+}
